6637739abb6f7acbe608acdfd28404f2199e61ad
VanoVasilyan <vanovasilyan@gmail.com>
2022-03-21 19:53:45 +0400

    fix path issue

--- src/pages/Accounts/index.jsx
+++ src/pages/Accounts/index.jsx
@@ -3,10 +3,17 @@ import { Table } from 'antd';
 import columns from './columns';
 import 'antd/dist/antd.min.css'
 import './Accounts.css'
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const Accounts = () => {
-    const { data } = useGlobalContext();
-    return <Table pagination={false} bordered={true} columns={columns} dataSource={data} />
+    const { data, loading } = useGlobalContext();
+    const antIcon = <LoadingOutlined style={{ fontSize: 50, position:'relative',  }} spin />;
+
+    return <>
+        {loading && <Spin indicator={antIcon} style={{ position: 'absolute', left: '50%', top: '30%', zIndex: '10' }}/>}
+        <Table pagination={false} bordered={true} columns={columns} dataSource={data} />
+    </>
 }
 
 export default Accounts
